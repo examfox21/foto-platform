f0036ed726aa20e89dacb0d41e75d56b2009f34a
examfox21 <examfox21@gmail.com>
2025-08-28 18:10:45 +0200

    Update layout.tsx

--- src/app/(photographer)/layout.tsx
+++ src/app/(photographer)/layout.tsx
@@ -97,8 +97,8 @@ export default function PhotographerLayout({
 
       {/* Sidebar */}
       <div className={`
-        fixed inset-y-0 left-0 z-50 w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:inset-0
-        ${isMobileMenuOpen ? 'translate-x-0' : '-translate-x-full'}
+        fixed inset-y-0 left-0 z-50 w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out lg:translate-x-0
+        ${isMobileMenuOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}
       `}>
         <div className="flex flex-col h-full">
           {/* Logo */}
